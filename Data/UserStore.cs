eaacbf8dff3f8d84882cdd8c91bdf3d27fac8f30
Rafif Sadid Hamdani <rafifgame2@gmail.com>
2023-11-16 15:53:39 +0700

    Add files via upload

---
+++ Data/UserStore.cs
@@ -0,0 +1,16 @@
+﻿using Identity.WebAPI.Models.Dto;
+
+namespace Identity.WebAPI.Data
+{
+    public static class UserStore
+    {
+        public static List<UserDTO> userList = new List<UserDTO>
+        {
+            new UserDTO{Id=1    , Username="Rafif Sadid Hamdani"    , Email="Rafif@gmail.com"   , Password="Rafif"},
+            new UserDTO{Id=2    , Username="Juanda Ritonga"         , Email="Juan@gmail.com"    , Password="Juan"},
+            new UserDTO{Id=3    , Username="Maulana Yusuf Suradin"  , Email="Yusuf@gmail.com"   , Password="Yusuf"},
+            new UserDTO{Id=4    , Username="Hammam Faiz"            , Email="Faiz@gmail.com"    , Password="Faiz"}
+        };
+
+    }
+}
